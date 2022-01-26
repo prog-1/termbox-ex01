ecd3121aa0856765f898343928a70827e5ae54dc
unknown <valerijamax@gmail.com>
2022-01-26 21:37:14 +0200

    3 steps up 3 steps down3 steps up 3 steps down3 steps up 3 steps down

--- 02_snake_animation/main.go
+++ 02_snake_animation/main.go
@@ -29,7 +29,7 @@ type snake struct {
 
 // newSnake returns a new struct instance representing a snake.
 func newSnake() snake {
-	return snake{coord{5, 5}, coord{1, 0}}
+	return snake{coord{5, 7}, coord{1, 0}}
 }
 
 // Redraws the terminal.
@@ -41,8 +41,12 @@ func drawSnake(s snake) {
 
 // Makes a move for a snake and returns a snake with an updated position.
 func moveSnake(s snake) snake {
-	s.pos.x += s.dir.x
 	s.pos.y += s.dir.y
+	if s.pos.x == 5 && s.pos.y == 7 {
+		s.dir.x, s.dir.y = 0, 1
+	} else if s.pos.x == 5 && s.pos.y == 10 {
+		s.dir.x, s.dir.y = 0, -1
+	}
 	return s
 }
 
