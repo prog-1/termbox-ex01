c5ffba5132429c84767fce65119ae1b971a0bef0
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2022-01-31 21:30:50 +0200

    did all the tasks except the 5th one (when the snake hits itself)

--- 04_snake_game/main.go
+++ 04_snake_game/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -11,9 +12,13 @@ import (
 
 const (
 	snakeBody    = '*'
-	snakeFgColor = termbox.ColorRed
+	snakeFgColor = termbox.ColorGreen
 	// Use the default background color for the snake.
 	snakeBgColor = termbox.ColorDefault
+
+	appleBody    = 'o'
+	appleFgColor = termbox.ColorYellow
+	appleBgColor = termbox.ColorDefault
 )
 
 // writeText writes a string to the buffer.
@@ -31,12 +36,18 @@ type coord struct {
 // snake is a struct with fields representing a snake.
 type snake struct {
 	// Position of a snake.
-	pos coord
+	pos []coord
+}
+
+type apple struct {
+	pos   coord
+	score int
 }
 
 // game represents a state of the game.
 type game struct {
 	sn snake
+	ap apple
 	v  coord
 	// Game field dimensions.
 	fieldWidth, fieldHeight int
@@ -45,10 +56,35 @@ type game struct {
 // newSnake returns a new struct instance representing a snake.
 // The snake is placed in a random position in the game field.
 // The movement direction is right.
-func newSnake(maxX, maxY int) snake {
+func newSnake(g game) snake {
 	// rand.Intn generates a pseudo-random number:
 	// https://pkg.go.dev/math/rand#Intn
-	return snake{coord{rand.Intn(maxX), rand.Intn(maxY)}}
+	x, y := rand.Intn(g.fieldWidth), rand.Intn(g.fieldHeight)
+
+	g.sn.pos = []coord{{x, y}, {x - 1, y}, {x - 2, y}}
+	for {
+		if g.sn.pos[0].x == 0 || g.sn.pos[0].x == g.fieldWidth-1 || g.sn.pos[0].y == 0 || g.sn.pos[0].y == 1 || g.sn.pos[0].y == g.fieldHeight-1 {
+			g.sn.pos = []coord{{x, y}, {x - 1, y}, {x - 2, y}}
+		} else {
+			break
+		}
+	}
+	return snake{g.sn.pos}
+}
+
+func newApple(g game) apple {
+	// g.ap.pos.x, g.ap.pos.y = rand.Intn(g.fieldWidth), rand.Intn(g.fieldHeight)
+	// return apple{coord{g.ap.pos.x, g.ap.pos.y}, g.ap.score}
+
+	g.ap.pos = coord{rand.Intn(g.fieldWidth), rand.Intn(g.fieldHeight)}
+	for {
+		if g.ap.pos.x == 0 || g.ap.pos.x == g.fieldWidth-1 || g.ap.pos.y == 0 || g.ap.pos.y == 1 || g.ap.pos.y == g.fieldHeight-1 {
+			g.ap.pos = coord{rand.Intn(g.fieldWidth), rand.Intn(g.fieldHeight)}
+		} else {
+			break
+		}
+	}
+	return apple{g.ap.pos, g.ap.score}
 }
 
 // newGame returns a new game state.
@@ -58,21 +94,58 @@ func newGame() game {
 	return game{
 		fieldWidth:  w,
 		fieldHeight: h,
-		sn:          newSnake(w, h),
-		v:           coord{1, 0},
+		// sn:       newSnake(w, h),
+		// ap:       newApple(w, h),
+		v: coord{1, 0},
 	}
 }
+
+// func mainMenuColor() (choice int) {
+//	   fmt.Println(`Please select the color of your snake:
+// 1) Green
+// 2) Orange
+// 3) Blue
+// 4) Exit`)
+//	   fmt.Scanln(&choice)
+//	   return
+// }
 
 // drawSnakePosition draws the current snake position (as a debugging
 // information) in the buffer.
 func drawSnakePosition(g game) {
-	str := fmt.Sprintf("(%d, %d)", g.sn.pos.x, g.sn.pos.y)
+	str := fmt.Sprintf("(%d, %d)", g.sn.pos[0].x, g.sn.pos[0].y)
 	writeText(g.fieldWidth-len(str), 0, str, snakeFgColor, snakeBgColor)
+}
+
+func drawScore(g game) {
+	str := fmt.Sprintf("(Score: %d)", g.ap.score)
+	writeText(g.fieldWidth/2, 0, str, termbox.ColorWhite, snakeBgColor)
+
 }
 
 // drawSnake draws the snake in the buffer.
 func drawSnake(sn snake) {
-	termbox.SetCell(sn.pos.x, sn.pos.y, snakeBody, snakeFgColor, snakeBgColor)
+	// termbox.SetCell(sn.pos.x, sn.pos.y, snakeBody, snakeFgColor, snakeBgColor)
+
+	for _, pos := range sn.pos {
+		termbox.SetCell(pos.x, pos.y, snakeBody, snakeFgColor, snakeBgColor)
+	}
+}
+
+func drawApple(a apple) {
+	termbox.SetCell(a.pos.x, a.pos.y, appleBody, appleFgColor, appleBgColor)
+
+}
+
+func drawBorders(g game) {
+	for i := 0; i < g.fieldWidth; i++ {
+		termbox.SetBg(i, 1, termbox.ColorRed)
+		termbox.SetBg(i, g.fieldHeight-1, termbox.ColorRed)
+	}
+	for j := 0; j < g.fieldHeight; j++ {
+		termbox.SetBg(0, j+1, termbox.ColorRed)
+		termbox.SetBg(g.fieldWidth-1, j+1, termbox.ColorRed)
+	}
 }
 
 // Redraws the terminal.
@@ -80,7 +153,10 @@ func draw(g game) {
 	// Clear the old "frame".
 	termbox.Clear(snakeFgColor, snakeBgColor)
 	drawSnakePosition(g)
+	drawScore(g)
 	drawSnake(g.sn)
+	drawApple(g.ap)
+	drawBorders(g)
 	// Update the "frame".
 	termbox.Flush()
 }
@@ -93,14 +169,37 @@ func mod(n, m int) int {
 
 // Makes a move for a snake. Returns a snake with an updated position.
 func moveSnake(s snake, v coord, fw, fh int) snake {
-	s.pos.x = mod(s.pos.x+v.x, fw)
-	s.pos.y = mod(s.pos.y+v.y, fh)
+	copy(s.pos[1:], s.pos[:])
+	s.pos[0] = coord{s.pos[0].x + v.x, s.pos[0].y + v.y}
 	return s
+}
+
+func HitsTheWalls(g game) bool {
+	if g.sn.pos[0].x == 0 || g.sn.pos[0].x == g.fieldWidth-1 || g.sn.pos[0].y == 1 || g.sn.pos[0].y == g.fieldHeight-1 {
+		writeText(g.fieldWidth/2, g.fieldHeight/2, "Game Over", termbox.ColorRed|termbox.AttrBold, termbox.ColorDefault)
+		termbox.Flush()
+		time.Sleep(5 * time.Second)
+		termbox.Clear(snakeFgColor, snakeBgColor)
+		termbox.Flush()
+		return true
+	}
+	return false
 }
 
 func step(g game) game {
 	g.sn = moveSnake(g.sn, g.v, g.fieldWidth, g.fieldHeight)
 	draw(g)
+
+	if g.sn.pos[0] == g.ap.pos {
+		g.ap.score++
+		g.ap = newApple(g)
+		g.sn.pos = append([]coord{{g.sn.pos[0].x, g.sn.pos[0].y}}, g.sn.pos...)
+	}
+
+	if HitsTheWalls(g) {
+		os.Exit(0)
+	}
+
 	return g
 }
 
@@ -108,6 +207,16 @@ func moveLeft(g game) game  { g.v = coord{-1, 0}; return g }
 func moveRight(g game) game { g.v = coord{1, 0}; return g }
 func moveUp(g game) game    { g.v = coord{0, -1}; return g }
 func moveDown(g game) game  { g.v = coord{0, 1}; return g }
+
+func mainMenuDifficulty() (choice int) {
+	fmt.Println(`Select difficulty:
+1) Easy
+2) Medium
+3) Hard
+4) Exit`)
+	fmt.Scanln(&choice)
+	return
+}
 
 // Tasks:
 func main() {
@@ -121,6 +230,8 @@ func main() {
 	// Other initialization.
 	rand.Seed(time.Now().UnixNano())
 	g := newGame()
+	g.sn = newSnake(g)
+	g.ap = newApple(g)
 
 	eventQueue := make(chan termbox.Event)
 	go func() {
@@ -129,7 +240,19 @@ func main() {
 		}
 	}()
 
-	ticker := time.NewTicker(70 * time.Millisecond)
+	var ticker *time.Ticker
+	choice := mainMenuDifficulty()
+	if choice == 1 {
+		ticker = time.NewTicker(120 * time.Millisecond)
+	} else if choice == 2 {
+		ticker = time.NewTicker(80 * time.Millisecond)
+	} else if choice == 3 {
+		ticker = time.NewTicker(50 * time.Millisecond)
+	} else if choice == 4 {
+		os.Exit(0)
+	} else {
+		fmt.Println("ERROR: wrong choice")
+	}
 	defer ticker.Stop()
 
 	// This is the main event loop.
