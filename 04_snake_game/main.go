c9db59d56d8197be9224754be41bb127e9a573cf
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-02-02 16:47:50 +0200

    theres random 5 part snake, hallucinogenic borders and score that always is 10

--- 04_snake_game/main.go
+++ 04_snake_game/main.go
@@ -11,35 +11,29 @@ import (
 
 const (
 	snakeBody    = '*'
-	snakeFgColor = termbox.ColorRed
-	// Use the default background color for the snake.
+	snakeFgColor = termbox.ColorGreen
 	snakeBgColor = termbox.ColorDefault
 )
 
-// writeText writes a string to the buffer.
-func writeText(x, y int, s string, fg, bg termbox.Attribute) {
+func writeText(x, y int, s string, fg, bg termbox.Attribute) { // writeText writes a string to the buffer.
 	for i, ch := range s {
 		termbox.SetCell(x+i, y, ch, fg, bg)
 	}
 }
 
-// coord is a coordinate on a plane.
-type coord struct {
+type coord struct { // coord is a coordinate on a plane.
 	x, y int
 }
 
-// snake is a struct with fields representing a snake.
-type snake struct {
-	// Position of a snake.
-	pos coord
+type snake struct { // snake is a struct with fields representing a snake.
+	pos [5]coord // Position of a snake.
+
 }
 
-// game represents a state of the game.
-type game struct {
-	sn snake
-	v  coord
-	// Game field dimensions.
-	fieldWidth, fieldHeight int
+type game struct { // game represents a state of the game.
+	sn                      snake
+	v                       coord
+	fieldWidth, fieldHeight int // Game field dimensions.
 }
 
 // newSnake returns a new struct instance representing a snake.
@@ -47,14 +41,13 @@ type game struct {
 // The movement direction is right.
 func newSnake(maxX, maxY int) snake {
 	// rand.Intn generates a pseudo-random number:
+	//return snake{[5]coord{{5, 5}, {4, 5}, {3, 5}, {2, 5}}}
 	// https://pkg.go.dev/math/rand#Intn
-	return snake{coord{rand.Intn(maxX), rand.Intn(maxY)}}
+	return snake{[5]coord{{rand.Intn(maxX), rand.Intn(maxY)}, {rand.Intn(maxX - 1), rand.Intn(maxY)}, {rand.Intn(maxX - 2), rand.Intn(maxY)}, {rand.Intn(maxX - 3), rand.Intn(maxY)}, {rand.Intn(maxX - 4), rand.Intn(maxY)}}}
 }
 
-// newGame returns a new game state.
-func newGame() game {
-	// Sets game field dimensions to the size of the terminal.
-	w, h := termbox.Size()
+func newGame() game { // newGame returns a new game state.
+	w, h := termbox.Size() // Sets game field dimensions to the size of the terminal.
 	return game{
 		fieldWidth:  w,
 		fieldHeight: h,
@@ -66,35 +59,56 @@ func newGame() game {
 // drawSnakePosition draws the current snake position (as a debugging
 // information) in the buffer.
 func drawSnakePosition(g game) {
-	str := fmt.Sprintf("(%d, %d)", g.sn.pos.x, g.sn.pos.y)
+	str := fmt.Sprintf("(%d, %d)", g.sn.pos[0].x, g.sn.pos[0].y)
 	writeText(g.fieldWidth-len(str), 0, str, snakeFgColor, snakeBgColor)
 }
-
-// drawSnake draws the snake in the buffer.
-func drawSnake(sn snake) {
-	termbox.SetCell(sn.pos.x, sn.pos.y, snakeBody, snakeFgColor, snakeBgColor)
+func drawScore(g game) {
+	temporaryScore := 10
+	str := fmt.Sprintf("Score: %d", temporaryScore)
+	writeText((g.fieldWidth/2)-len(str), 0, str, snakeFgColor, snakeBgColor)
+}
+func drawHearts(g game) { // in process
+	heart := "♥"
+	str := fmt.Sprintf("%s%s%s", heart, heart, heart)
+	writeText(0, 0, str, snakeFgColor, snakeBgColor)
 }
 
-// Redraws the terminal.
-func draw(g game) {
-	// Clear the old "frame".
-	termbox.Clear(snakeFgColor, snakeBgColor)
+func drawSnake(sn snake) { // drawSnake draws the snake in the buffer.
+	for _, pos := range sn.pos {
+		termbox.SetCell(pos.x, pos.y, snakeBody, snakeFgColor, snakeBgColor)
+	}
+}
+func drawBorder(g game) {
+	ch := rune('#')
+	bColor := termbox.ColorCyan
+	bBackC := termbox.ColorDefault
+	for i := 0; i != g.fieldWidth; i++ {
+		termbox.SetCell(i, 1, ch, bColor, bBackC)
+	}
+	for i := 1; i != g.fieldHeight; i++ {
+		termbox.SetCell(0, i, ch, bColor, bBackC)
+	}
+	for i := g.fieldWidth; i != 0; i-- {
+		termbox.SetCell(i, g.fieldHeight-1, ch, bColor, bBackC)
+	}
+	for i := g.fieldHeight; i != 0; i-- {
+		termbox.SetCell(g.fieldWidth-1, i, ch, bColor, bBackC)
+	}
+}
+
+func draw(g game) { // Redraws the terminal.
+	termbox.Clear(snakeFgColor, snakeBgColor) // Clear the old "frame".
+	drawBorder(g)
+	drawHearts(g)
+	drawScore(g)
 	drawSnakePosition(g)
 	drawSnake(g.sn)
-	// Update the "frame".
-	termbox.Flush()
+	termbox.Flush() // Update the "frame".
 }
 
-// mod is a custom implementation of the '%' (modulo) operator that always
-// returns positive numbers.
-func mod(n, m int) int {
-	return (n%m + m) % m
-}
-
-// Makes a move for a snake. Returns a snake with an updated position.
-func moveSnake(s snake, v coord, fw, fh int) snake {
-	s.pos.x = mod(s.pos.x+v.x, fw)
-	s.pos.y = mod(s.pos.y+v.y, fh)
+func moveSnake(s snake, v coord, fw, fh int) snake { // Makes a move for a snake. Returns a snake with an updated position.
+	copy(s.pos[1:], s.pos[:])
+	s.pos[0] = coord{s.pos[0].x + v.x, s.pos[0].y + v.y}
 	return s
 }
 
@@ -109,17 +123,15 @@ func moveRight(g game) game { g.v = coord{1, 0}; return g }
 func moveUp(g game) game    { g.v = coord{0, -1}; return g }
 func moveDown(g game) game  { g.v = coord{0, 1}; return g }
 
-// Tasks:
 func main() {
-	// Initialize termbox.
-	err := termbox.Init()
+	err := termbox.Init() // Initialize termbox.
 	if err != nil {
 		log.Fatalf("failed to init termbox: %v", err)
 	}
 	defer termbox.Close()
 
-	// Other initialization.
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano()) // Other initialization.
+
 	g := newGame()
 
 	eventQueue := make(chan termbox.Event)
@@ -129,11 +141,10 @@ func main() {
 		}
 	}()
 
-	ticker := time.NewTicker(70 * time.Millisecond)
+	ticker := time.NewTicker(150 * time.Millisecond)
 	defer ticker.Stop()
 
-	// This is the main event loop.
-	for {
+	for { // This is the main event loop.
 		select {
 		case ev := <-eventQueue:
 			if ev.Type == termbox.EventKey {
